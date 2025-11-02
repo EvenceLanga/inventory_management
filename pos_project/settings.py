649bed8b7cb4f83708fe023d53d51315185bc816
Evence Mohau Langa <evencel@netninenine.co.za>
2025-11-02 17:37:10 +0200

    Add host

--- pos_project/settings.py
+++ pos_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9w+#hdh+)&g258dvqk=!#h(_o!me&+bexwdt6+*_7z6l%&z_(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory-management-hsed.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
